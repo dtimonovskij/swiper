a8f8284a513a45c4ddee9bd5b9ba5f6fb37813f2
dtimonovskij <dtimonovskij@gmail.com>
2020-06-13 17:23:01 +0300

    swiper done

--- js/main.js
+++ js/main.js
@@ -4,13 +4,25 @@ toggle[0].addEventListener("click", function(event) {
   event.preventDefault();
   this.classList.toggle("is-active");
 });
+
+const closeNavbarMobile = document.getElementsByClassName("close");
+
 closeNavbarMobile[0].addEventListener("click", function(event) {
   event.preventDefault();
   toggle[0].classList.remove("is-active");
 });
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.first-swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+});
+var swiperSecondary = new Swiper('.second-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
 });
